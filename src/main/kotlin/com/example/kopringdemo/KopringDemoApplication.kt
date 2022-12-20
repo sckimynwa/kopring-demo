bf3f4ccbfd398add5faee87515c38517da043e73
sckimynwa <tsb03672@naver.com>
2022-12-20 11:08:51 +0900

    feat: add sammple db connection (in-memory)

--- src/main/kotlin/com/example/kopringdemo/KopringDemoApplication.kt
+++ src/main/kotlin/com/example/kopringdemo/KopringDemoApplication.kt
@@ -2,8 +2,13 @@ package com.example.kopringdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @SpringBootApplication
 class KopringDemoApplication
@@ -13,13 +18,26 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageController {
+class MessageController(val service: MessageService) {
     @GetMapping("/")
-    fun index() = listOf(
-        Message("1", "Hello!"),
-        Message("2", "Bonjour!"),
-        Message("3", "Privet!"),
-    )
+    fun index(): List<Message> = service.findMessages()
+
+    @PostMapping("/")
+    fun post(@RequestBody message: Message) {
+        service.save(message)
+    }
+}
+
+@Service
+class MessageService(val db: JdbcTemplate) {
+    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun save(message: Message) {
+        val id = message.id ?: UUID.randomUUID().toString()
+        db.update("insert into messages values (?, ?)", id, message.text)
+    }
 }
 
 data class Message(val id: String?, val text: String)
