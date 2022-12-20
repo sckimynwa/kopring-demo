9cce158618985e7b85adcdc3080f8aabd915c119
sckimynwa <tsb03672@naver.com>
2022-12-20 10:36:03 +0900

    feat: add sample data class

--- src/main/kotlin/com/example/kopringdemo/KopringDemoApplication.kt
+++ src/main/kotlin/com/example/kopringdemo/KopringDemoApplication.kt
@@ -3,7 +3,6 @@ package com.example.kopringdemo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
@@ -16,5 +15,11 @@ fun main(args: Array<String>) {
 @RestController
 class MessageController {
     @GetMapping("/")
-    fun index(@RequestParam("name") name: String) = "hello, $name!"
+    fun index() = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
 }
+
+data class Message(val id: String?, val text: String)
