5667f750257482625a76cdefedc169747a38e789
sckimynwa <tsb03672@naver.com>
2022-12-20 10:09:57 +0900

    feat: initialize demo project

---
+++ src/main/kotlin/com/example/kopringdemo/KopringDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kopringdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringDemoApplication>(*args)
+}
