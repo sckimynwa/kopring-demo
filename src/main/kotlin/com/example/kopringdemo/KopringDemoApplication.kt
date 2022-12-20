fa8d77942c0420a707a4c99ae9fafe33f2786f12
sckimynwa <tsb03672@naver.com>
2022-12-20 10:12:53 +0900

    feat: add sample controller

--- src/main/kotlin/com/example/kopringdemo/KopringDemoApplication.kt
+++ src/main/kotlin/com/example/kopringdemo/KopringDemoApplication.kt
@@ -2,10 +2,19 @@ package com.example.kopringdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class KopringDemoApplication
 
 fun main(args: Array<String>) {
     runApplication<KopringDemoApplication>(*args)
+}
+
+@RestController
+class MessageController {
+    @GetMapping("/")
+    fun index(@RequestParam("name") name: String) = "hello, $name!"
 }
