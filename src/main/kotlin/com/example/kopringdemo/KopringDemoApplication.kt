4a843994511d07a5fd0f2327434f1da47f4d004a
sckimynwa <tsb03672@naver.com>
2022-12-28 09:01:54 +0900

    feat: remove temp dbconnection

--- src/main/kotlin/com/example/kopringdemo/KopringDemoApplication.kt
+++ src/main/kotlin/com/example/kopringdemo/KopringDemoApplication.kt
@@ -6,10 +6,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @SpringBootApplication
 class KopringDemoApplication
@@ -19,26 +16,38 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageController(val service: MessageService) {
+class MessageController() {
     @GetMapping("/")
-    fun index(): List<Message> = service.findMessages()
-
-    @PostMapping("/")
-    fun post(@RequestBody message: Message) {
-        service.save(message)
-    }
+    fun index(@RequestParam("name") name: String) = "Hello, $name!"
 }
 
-@Table("MESSAGES")
-data class Message(@Id val id: String?, val text: String)
+//    @PostMapping("/")
+//    fun post(@RequestBody message: Message) {
+//        service.save(message)
+//    }
 
-interface MessageRepository: CrudRepository<Message, String>
 
-@Service
-class MessageService(val db: MessageRepository) {
-    fun findMessages(): List<Message> = db.findAll().toList()
+//@RestController
+//class MessageController(val service: MessageService) {
+//    @GetMapping("/")
+//    fun index(): List<Message> = service.findMessages()
+//
+//    @PostMapping("/")
+//    fun post(@RequestBody message: Message) {
+//        service.save(message)
+//    }
+//}
 
-    fun save(message: Message) {
-        db.save(message)
-    }
-}
+//@Table("MESSAGES")
+//data class Message(@Id val id: String?, val text: String)
+//
+//interface MessageRepository: CrudRepository<Message, String>
+//
+//@Service
+//class MessageService(val db: MessageRepository) {
+//    fun findMessages(): List<Message> = db.findAll().toList()
+//
+//    fun save(message: Message) {
+//        db.save(message)
+//    }
+//}
